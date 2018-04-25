09d159d2b87223ba6e0db5de0f13b48e2e5936a7
aelsv <alexander.elsv@gmail.com>
2018-04-25 11:13:03 +0300

    Weather reducer provide error msg

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,33 +1,36 @@
 const FETCH_WEATHER = 'FETCH_WEATHER';
+const FETCH_FAILURE = 'FETCH_FAILURE';
 
 const API_KEY = '160fbb3243196b25c6802250eb628e96';
-
 const rootURL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
 
 const initialState = {
   queries: [],
-  temperature: {},
+  error: '',
+  // isFething: false,
 };
 
-export default (state = initialState, action = {}) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_WEATHER:
-      const { id, name, sys, main, weather /* , cod, message */ } = action.payload;
+      const { id, name, sys, main, weather, error } = action.payload;
       return {
         ...state,
-        // ...action.payload,
-        queries: !!id && !!name ? [{ id, name }, ...state.queries] : [...state.queries],
-        icon: !!weather ? weather[0].icon : '',
-        briefDescription: !!weather ? weather[0].description : '',
+        error,
+        queries: [{ id, name }, ...state.queries],
+        briefDescription: weather[0].description,
         city: name,
-        country: !!sys ? sys.country : '',
-        temperature: !!main
-          ? {
-              current: main.temp,
-              min: main.temp_min,
-              max: main.temp_max,
-            }
-          : {},
+        country: sys.country,
+        temperature: {
+          current: main.temp,
+          min: main.temp_min,
+          max: main.temp_max,
+        },
+      };
+    case FETCH_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
@@ -35,18 +38,26 @@ export default (state = initialState, action = {}) => {
 };
 
 export const fetchWeather = city => dispatch => ({
-  // const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  // return {
-  type: FETCH_WEATHER,
-  // promise: fetch(URL)
-  promise: fetch(`${rootURL}&q=${city}&units=metric`)
-    .then(res => res.json())
-    .then(json =>
-      dispatch({
-        type: FETCH_WEATHER,
-        payload: json,
+  types: [FETCH_WEATHER, FETCH_FAILURE],
+  promise: fetch(`${rootURL}&q=${city}&units=metric`).then(res =>
+    res
+      .json()
+      .then(json => ({
+        ok: res.ok,
+        json,
+      }))
+      .then(({ ok, json }) => {
+        if (ok) {
+          dispatch({
+            type: FETCH_WEATHER,
+            payload: json,
+          });
+        } else {
+          dispatch({
+            type: FETCH_FAILURE,
+            payload: json.message || 'Something went wrong!',
+          });
+        }
       })
-    ),
-  // .catch(error => console.log('error:', error)),
-  // };
+  ),
 });
